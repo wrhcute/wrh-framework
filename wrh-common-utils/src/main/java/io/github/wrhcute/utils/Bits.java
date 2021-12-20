8192d05cb841e1783045d00cf2358ba2bc35c995
wrh <wangruihong@qxdaojia.com>
2021-12-20 10:12:14 +0800

    基本工具类

--- wrh-common-utils/src/main/java/io/github/wrhcute/utils/Bits.java
+++ wrh-common-utils/src/main/java/io/github/wrhcute/utils/Bits.java
@@ -11,7 +11,8 @@ public abstract class Bits {
 
     //传入数值是否为2的幂次方数
     public static boolean isPowerOf2(int num) {
-        return num != 1 && (num & (num - 1)) == 0;
+        int abs = Math.abs(num);
+        return abs != 1 && (abs & (abs - 1)) == 0;
     }
 
 }
