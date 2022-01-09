ed67d93c39e3af36b68c51185bb15f6e3c997d4d
code23333 <2630369116@qq.com>
2022-01-09 22:48:45 +0800

    NumID

--- wrh-common-utils/src/main/java/io/github/wrhcute/utils/NumID.java
+++ wrh-common-utils/src/main/java/io/github/wrhcute/utils/NumID.java
@@ -3,19 +3,19 @@ package io.github.wrhcute.utils;
 /**
  * @author 王瑞鸿
  * @version 1.0.0
- * @ClassName IdGenerator.java
+ * @ClassName NumID.java
  * @Description id生成器
  *
  * @createTime 2021年11月17日 15:26:00
  */
-public class IdGenerator {
+public class NumID {
 
     private final long fixed;
     private volatile long lastTimestamp = System.currentTimeMillis();
     private volatile int seq = 0;
     private static final int MAX_SEQ = 4096; //2的12次方
 
-    public IdGenerator(int fixed) {
+    public NumID(int fixed) {
         Asserts.isTrue(fixed >= 0 && fixed <= 1024, "fixed必须大于等于0小于等于1024");
         this.fixed = (long) fixed << 53;
     }
--- wrh-common-utils/src/test/java/io/github/wrhcute/utils/NumIDTest.java
+++ wrh-common-utils/src/test/java/io/github/wrhcute/utils/NumIDTest.java
@@ -5,9 +5,8 @@ import org.junit.Test;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.LockSupport;
 
-public class IdGeneratorTest {
+public class NumIDTest {
 
 
 
@@ -15,11 +14,11 @@ public class IdGeneratorTest {
     public void test(){
         ExecutorService executorService = Executors.newFixedThreadPool(20);
         ConcurrentHashMap<Long,Long> map = new ConcurrentHashMap<>();
-        IdGenerator idGenerator = new IdGenerator(89);
+        NumID numID = new NumID(89);
         for (int i = 0; i < 20; i++) {
            executorService.submit(() -> {
                for (;;){
-                   long id = idGenerator.take();
+                   long id = numID.take();
                    Long put = map.put(id, id);
                    System.out.println("线程"+ Thread.currentThread().getName() + "放入 " + id);
                    if (put != null){
