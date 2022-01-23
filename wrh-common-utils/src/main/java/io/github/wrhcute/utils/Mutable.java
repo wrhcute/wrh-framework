17b3187f90f9fcd99f0c0f316e1efd992a01a944
wrh <wangruihong@qxdaojia.com>
2022-01-23 19:05:16 +0800

    nothing

--- wrh-common-utils/src/main/java/io/github/wrhcute/utils/Mutable.java
+++ wrh-common-utils/src/main/java/io/github/wrhcute/utils/Mutable.java
@@ -2,11 +2,15 @@ package io.github.wrhcute.utils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
+@SuppressWarnings("unchecked")
 public class Mutable {
 
     private Map<String, Prop> propMap = new HashMap<>();
@@ -46,7 +50,14 @@ public class Mutable {
         List<Field> fields = Reflections.getFields(clazz);
         for (Field field : fields) {
             String name = getPropName(field);
-            this.propMap.put(name, new Prop(owner -> Reflections.getFieldValue(field, owner), obj));
+            this.propMap.put(name, new Prop(owner -> {
+                Method getter = Reflections.findGetter(clazz, field.getName());
+                if (getter != null){
+                    return Reflections.callMethod(getter,obj);
+                }else{
+                    return Reflections.getFieldValue(field, owner);
+                }
+            }, obj));
         }
     }
 
@@ -84,10 +95,9 @@ public class Mutable {
                 if (targetType == sourceType || sourceType.isAssignableFrom(targetType)) {
                     Object targetValue = prop.originValue();
                     setTargetField(field, instance, targetValue);
-                } else if (strict) {
-                    continue;
-                } else {
-
+                } else if (!strict){
+                    Object targetValue = tryConvert(prop.originValue(),field.getType());
+                    setTargetField(field, instance, targetValue);
                 }
             }
         }
@@ -104,9 +114,50 @@ public class Mutable {
         }
     }
 
+    private static <T> T tryConvert(Object obj , Class<T> target){
+        Class<?> clz = obj.getClass();
+        if (target == String.class){
+            Method toString = Reflections.getMethod(clz,"toString");
+            if (toString.getDeclaringClass() != Object.class)//有重写toString
+                return (T) obj.toString();
+        }else if (target.isAssignableFrom(Number.class)){
+            if (clz.isAssignableFrom(Number.class)){
+                Number number = ((Number) obj);
+                return (T) numberConvert(number,(Class<? extends Number>) target);
+            }else if (clz.isArray()){
+                return (T)Integer.valueOf(((Object[]) obj).length);
+            }else if (clz.isAssignableFrom(Collection.class)){
+                int size = ((Collection) obj).size();
+                return Integer.valueOf(size);
+            }
+
+        }
+        return null;
+    }
+
+
+    private static <T extends Number> T numberConvert(Number number ,Class<T> target){
+        if (target == Long.class || target == long.class)
+            return (T) Long.valueOf(number.longValue());
+        else if (target == Integer.class || target == int.class)
+            return (T) Integer.valueOf(number.intValue());
+        else if (target == Double.class || target == double.class)
+            return (T) Double.valueOf(number.doubleValue());
+        else if (target == Float.class || target == float.class)
+            return (T) Float.valueOf(number.floatValue());
+        else if (target == Short.class || target == short.class)
+            return (T) Short.valueOf(number.shortValue());
+        else if (target == BigInteger.class)
+            return (T) BigInteger.valueOf(number.longValue());
+        else if (target == BigDecimal.class)
+            return (T) BigDecimal.valueOf(number.doubleValue());
+        else
+            throw new ClassCastException(number.getClass().getSimpleName() + "不能转换为" + target.getSimpleName());
+    }
+
     private static class Prop {
         private final Function<Object, Object> sourceFunc;
-        //属性拥有者
+
         private final Object owner;
 
         public Prop(Function<Object, Object> sourceFunc, Object owner) {
